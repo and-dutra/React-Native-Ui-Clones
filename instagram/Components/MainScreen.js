1593ca3f5eac4c2a179241dacaffe48eb179203b
André D Oliveira <decaobr@gmail.com>
2018-11-30 21:51:50 -0200

    Stories Header

--- instagram/Components/MainScreen.js
+++ instagram/Components/MainScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
-import { Icon, Container, Content } from "native-base";
+import { Text, StyleSheet, View, ScrollView } from "react-native";
+import { Icon, Container, Content, Thumbnail } from "native-base";
 import CardComponent from "./CardComponent";
 
 export default class MainScreen extends Component {
@@ -13,6 +13,93 @@ export default class MainScreen extends Component {
     return (
       <Container style={styles.container}>
         <Content>
+          <View style={{ height: 100 }}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingHorizontal: 7
+              }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Stories</Text>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Icon style={{ fontSize: 14 }} name="ios-play" />
+                <Text style={{ fontWeight: "bold", marginLeft: 3 }}>
+                  Watch All
+                </Text>
+              </View>
+            </View>
+            <View style={{ flex: 3 }}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                  alignItems: "center",
+                  paddingStart: 5,
+                  paddingEnd: 5
+                }}
+              >
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 2,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../assets/StoriesHeaderThumbnails/1.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 2,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../assets/StoriesHeaderThumbnails/2.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 2,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../assets/StoriesHeaderThumbnails/3.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 2,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../assets/StoriesHeaderThumbnails/4.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 2,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../assets/StoriesHeaderThumbnails/5.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 2,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../assets/StoriesHeaderThumbnails/6.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 2,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../assets/StoriesHeaderThumbnails/7.jpg")}
+                />
+              </ScrollView>
+            </View>
+          </View>
           <CardComponent imageSource="1" likes="101" />
           <CardComponent imageSource="2" likes="300" />
           <CardComponent imageSource="3" likes="101" />
